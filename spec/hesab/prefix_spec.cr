a1e19a10f99676b55476caab03b952e447e7cbbf
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2018-02-25 17:14:12 +0330

    Separated unit prefixes into another file

---
+++ spec/hesab/prefix_spec.cr
@@ -0,0 +1,73 @@
+require "../spec_helper"
+
+describe Prefix do
+  context Prefix::PREFIX do
+    it "should have atto" do
+      Prefix::PREFIX[:a].should eq(1e-18)
+    end
+
+    it "should have femto" do
+      Prefix::PREFIX[:f].should eq(1e-15)
+    end
+
+    it "should have pico" do
+      Prefix::PREFIX[:p].should eq(1e-12)
+    end
+
+    it "should have nano" do
+      Prefix::PREFIX[:n].should eq(1e-9)
+    end
+
+    it "should have micro" do
+      Prefix::PREFIX[:u].should eq(1e-6)
+    end
+
+    it "should have milli" do
+      Prefix::PREFIX[:m].should eq(1e-3)
+    end
+
+    it "should have centi" do
+      Prefix::PREFIX[:c].should eq(1e-2)
+    end
+
+    it "should have deci" do
+      Prefix::PREFIX[:d].should eq(1e-1)
+    end
+
+    it "should have one" do
+      Prefix::PREFIX[:one].should eq(1e0)
+    end
+
+    it "should have deca" do
+      Prefix::PREFIX[:da].should eq(1e1)
+    end
+
+    it "should have hecto" do
+      Prefix::PREFIX[:h].should eq(1e2)
+    end
+
+    it "should have kilo" do
+      Prefix::PREFIX[:k].should eq(1e3)
+    end
+
+    it "should have mega" do
+      Prefix::PREFIX[:M].should eq(1e6)
+    end
+
+    it "should have giga" do
+      Prefix::PREFIX[:G].should eq(1e9)
+    end
+
+    it "should have tera" do
+      Prefix::PREFIX[:T].should eq(1e12)
+    end
+
+    it "should have peta" do
+      Prefix::PREFIX[:P].should eq(1e15)
+    end
+
+    it "should have exa" do
+      Prefix::PREFIX[:E].should eq(1e18)
+    end
+  end
+end
---
+++ src/hesab/prefix.cr
@@ -0,0 +1,21 @@
+class Prefix
+  PREFIX = {
+    a:   1e-18,
+    f:   1e-15,
+    p:   1e-12,
+    n:   1e-9,
+    u:   1e-6,
+    m:   1e-3,
+    c:   1e-2,
+    d:   1e-1,
+    one: 1e0,
+    da:  1e1,
+    h:   1e2,
+    k:   1e3,
+    M:   1e6,
+    G:   1e9,
+    T:   1e12,
+    P:   1e15,
+    E:   1e18,
+  }
+end

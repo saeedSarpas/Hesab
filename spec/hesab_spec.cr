53d2efe385b0c13b16d916e71f1680c06945b792
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2018-02-25 10:37:45 +0330

    Put hesab tests in pending and also crystal spec needs class name

--- spec/hesab_spec.cr
+++ spec/hesab_spec.cr
@@ -1,19 +1,19 @@
-require 'spec_helper'
+require "./spec_helper"
 
 describe(Adad) do
-  it 'is accessible' do
-    expect(described_class.new 1.23).to be_a(Adad)
+  pending "is accessible" do
+    expect(Adad.new 1.23).to be_a(Adad)
   end
 end
 
 describe(Bordar) do
-  it 'is accessible' do
-    expect(described_class.new 1.23, [1]).to be_a(Bordar)
+  pending "is accessible" do
+    expect(Adad.new 1.23, [1]).to be_a(Bordar)
   end
 end
 
 describe(Formul) do
-  it 'is accessible' do
-    expect(described_class.new Adad.new(1.23)).to be_a(Formul)
+  pending "is accessible" do
+    expect(Adad.new Adad.new(1.23)).to be_a(Formul)
   end
 end

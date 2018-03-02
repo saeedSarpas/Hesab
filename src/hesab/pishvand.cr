2ac5181d2f015164f3b45160e944b79f8704f875
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2018-03-02 10:42:39 +0300

    Refactored constants in Pishvand to a module to isolate them

--- src/hesab/pishvand.cr
+++ src/hesab/pishvand.cr
@@ -4,34 +4,35 @@ struct Pishvand
   def initialize(@symbol : Symbol, @value : Float64)
   end
 
-  Yocto = self.new :y, 1e-24
-  Zepto = self.new :z, 1e-21
-  Atto  = self.new :a, 1e-18
-  Femto = self.new :f, 1e-15
-  Pico  = self.new :p, 1e-12
-  Nano  = self.new :n, 1e-9
-  Micro = self.new :u, 1e-6
-  Milli = self.new :m, 1e-3
-  Centi = self.new :c, 1e-2
-  Deci  = self.new :d, 1e-1
-  One   = self.new :one, 1e0
-  Deca  = self.new :da, 1e1
-  Hecto = self.new :h, 1e2
-  Kilo  = self.new :k, 1e3
-  Mega  = self.new :M, 1e6
-  Giga  = self.new :G, 1e9
-  Tera  = self.new :T, 1e12
-  Peta  = self.new :P, 1e15
-  Exa   = self.new :E, 1e18
-  Zetta = self.new :Z, 1e21
-  Yotta = self.new :Y, 1e24
+  module Constants
+    Yocto = Pishvand.new :y, 1e-24
+    Zepto = Pishvand.new :z, 1e-21
+    Atto  = Pishvand.new :a, 1e-18
+    Femto = Pishvand.new :f, 1e-15
+    Pico  = Pishvand.new :p, 1e-12
+    Nano  = Pishvand.new :n, 1e-9
+    Micro = Pishvand.new :u, 1e-6
+    Milli = Pishvand.new :m, 1e-3
+    Centi = Pishvand.new :c, 1e-2
+    Deci  = Pishvand.new :d, 1e-1
+    One   = Pishvand.new :one, 1e0
+    Deca  = Pishvand.new :da, 1e1
+    Hecto = Pishvand.new :h, 1e2
+    Kilo  = Pishvand.new :k, 1e3
+    Mega  = Pishvand.new :M, 1e6
+    Giga  = Pishvand.new :G, 1e9
+    Tera  = Pishvand.new :T, 1e12
+    Peta  = Pishvand.new :P, 1e15
+    Exa   = Pishvand.new :E, 1e18
+    Zetta = Pishvand.new :Z, 1e21
+    Yotta = Pishvand.new :Y, 1e24
 
-  def self.consts
-    {{ @type.constants.map { |c| @type.constant(c) }
-                      .select { |c| c.class_name == "Call" &&
-         c.receiver.id == "self" &&
-         c.name == "new" } }}
+    def self.consts
+      {{ @type.constants }}
+    end
   end
 
-  CONSTS = self.consts.map { |c| {c.symbol, c} }.to_h
+  include Constants
+
+  CONSTS = Constants.consts.map { |c| {c.symbol, c} }.to_h
 end
